3b26a516d2e14e81061ab9c4eb64882c4db912e2
Ahmed Elesawy <a7med.elesawy0155066@gmail.com>
2023-11-11 15:21:05 +0200

    fixed release

--- lib/widgets/search-widget.dart
+++ lib/widgets/search-widget.dart
@@ -12,31 +12,29 @@ class SearchWidget extends StatelessWidget {
   Widget build(BuildContext context) {
     final subProvider = Provider.of<WeatherProvider>(context, listen: false);
     return SizedBox(
-      child: Expanded(
-        child: Container(
-          margin: const EdgeInsets.fromLTRB(30, 70, 30, 30),
-          decoration: BoxDecoration(
-              color: Colors.white, borderRadius: BorderRadius.circular(30)),
-          child: TextField(
-              controller: searchController,
-              // onChanged: (value) =>
-              //     subProvider.fetchData(city: value),
-              onEditingComplete: () {
-                if (searchController.text != '') {
-                  subProvider.fetchData(city: searchController.text);
-                  FocusScope.of(context).unfocus();
-                }
+      child: Container(
+        margin: const EdgeInsets.fromLTRB(30, 70, 30, 30),
+        decoration: BoxDecoration(
+            color: Colors.white, borderRadius: BorderRadius.circular(30)),
+        child: TextField(
+            controller: searchController,
+            // onChanged: (value) =>
+            //     subProvider.fetchData(city: value),
+            onEditingComplete: () {
+              if (searchController.text != '') {
+                subProvider.fetchData(city: searchController.text);
                 FocusScope.of(context).unfocus();
-              },
-              decoration: InputDecoration(
-                  border: InputBorder.none,
-                  hintText: 'Search',
-                  icon: IconButton(
-                      onPressed: () {},
-                      icon: const Icon(
-                        Icons.search,
-                      )))),
-        ),
+              }
+              FocusScope.of(context).unfocus();
+            },
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                hintText: 'Search',
+                icon: IconButton(
+                    onPressed: () {},
+                    icon: const Icon(
+                      Icons.search,
+                    )))),
       ),
     );
   }
